1cfd6cdbb3f54e8b545dfd0c37166320f4b65c0a
Rob Kelly <rkelly@nmt.edu>
2014-08-22 17:08:56 -0600

    Changed autosave behavior - new images are saved to the directory dated with the date the observation began, rather than the current date (i.e. it doesn't create a new directory suddenly after midnight)

--- gui/guidepaneltab/guidecampanel.py
+++ gui/guidepaneltab/guidecampanel.py
@@ -19,107 +19,112 @@ import os
 import datetime
 
 AUTOSAVE_DIR = os.path.expanduser('~/Images/guide/%Y-%d-%m')
-AUTOSAVE_FNAME = AUTOSAVE_DIR + '/%H:%M:%S:%f.fits'
+AUTOSAVE_FORMAT = '/%a_%H:%M:%S:%f.fits'
 
-class GuideCamPanel(wx.Panel):    
+
+class GuideCamPanel(wx.Panel):
     def __init__(self, parent):
         super(GuideCamPanel, self).__init__(parent)
 
         self.parent = parent
 
-        #Exposure Time
+        # Exposure Time
         ################################################################
         self.exp_text = wx.StaticText(self, -1, label="Exposure (s):")
-        self.exposure = wx.TextCtrl(self, -1, '', size=(50,-1), 
+        self.exposure = wx.TextCtrl(self, -1, '', size=(50, -1),
                                     style=wx.TE_NO_VSCROLL)
         self.exposure.SetValue('0.25')
 
         #Take Image Button
         ################################################################
-        self.take = wx.Button(self, size=(100, -1), 
+        self.take = wx.Button(self, size=(100, -1),
                               label="Take Image")
 
         #Binning
         ################################################################
         self.bin_text = wx.StaticText(self, -1, label="Bin (NxN): ")
-        self.bin      = wx.SpinCtrl(self, id=-1, value='', 
-                                    min=1, max=4, initial=1, 
-                                    size=(40, -1))
-        
+        self.bin = wx.SpinCtrl(self, id=-1, value='',
+                               min=1, max=4, initial=1,
+                               size=(40, -1))
+
         #Series Controll Settings
         ################################################################
-        self.series        = wx.SpinCtrl(self, id=-1, value='', min=1, 
-                                        max=1000, initial=3, 
-                                        size=(50, -1))
-        self.series_text   = wx.StaticText(self, -1, label="Series: ")
+        self.series = wx.SpinCtrl(self, id=-1, value='', min=1,
+                                  max=1000, initial=3,
+                                  size=(50, -1))
+        self.series_text = wx.StaticText(self, -1, label="Series: ")
         self.scadence_text = wx.StaticText(self, -1, label="Cadence (s):")
-        self.scadence      = wx.TextCtrl(self, -1, '', size=(50,-1), 
-                                         style=wx.TE_NO_VSCROLL)
+        self.scadence = wx.TextCtrl(self, -1, '', size=(50, -1),
+                                    style=wx.TE_NO_VSCROLL)
         self.scadence.SetValue('2.0')
 
-        self.sname            = wx.TextCtrl(self, -1, 'img', size=(130,-1), 
-                                            style=wx.TE_NO_VSCROLL)
-        self.sname_text       = wx.StaticText(self, -1, 
-                                              label="Series Name: ")
+        self.sname = wx.TextCtrl(self, -1, 'img', size=(130, -1),
+                                 style=wx.TE_NO_VSCROLL)
+        self.sname_text = wx.StaticText(self, -1,
+                                        label="Series Name: ")
         self.autosave_on_text = wx.StaticText(self, label="Autosave: ")
-        self.autosave_cb      = wx.CheckBox(self, -1, " " , (10,10))
+        self.autosave_cb = wx.CheckBox(self, -1, " ", (10, 10))
         self.autosave_cb.SetValue(False)
 
 
         #Take Series Button
         ################################################################
-        self.take_series = wx.ToggleButton(self, 2, size=(100, -1), 
+        self.take_series = wx.ToggleButton(self, 2, size=(100, -1),
                                            label="Take Series")
 
 
         #Lighting Radio Buttons
         ################################################################
-        self.rb_light = wx.RadioButton(self, -1, 'Light', (10, 10), 
+        self.rb_light = wx.RadioButton(self, -1, 'Light', (10, 10),
                                        style=wx.RB_GROUP)
-        self.rb_dark  = wx.RadioButton(self, -1, 'Dark', (10, 30))
-        self.rb_flat  = wx.RadioButton(self, -1, 'Flat', (10, 50))
+        self.rb_dark = wx.RadioButton(self, -1, 'Dark', (10, 30))
+        self.rb_flat = wx.RadioButton(self, -1, 'Flat', (10, 50))
+
+        # Setup Autosave Directory
+        ################################################################
+        self.autosave_directory = datetime.datetime.now().strftime(AUTOSAVE_DIR)
 
         self._DoLayout()
         self._DoBindings()
 
     def _DoLayout(self):
-        sz     = wx.GridBagSizer(vgap=3, hgap=3)
+        sz = wx.GridBagSizer(vgap=3, hgap=3)
 
-        sz.Add(self.exp_text, pos=(0,0), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
-        sz.Add(self.exposure, pos=(0,1), span=(1,1), 
+        sz.Add(self.exp_text, pos=(0, 0), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT)
+        sz.Add(self.exposure, pos=(0, 1), span=(1, 1),
                flag=wx.ALIGN_CENTER_VERTICAL)
-        sz.Add(self.take, pos=(0,2), span=(1,1), 
+        sz.Add(self.take, pos=(0, 2), span=(1, 1),
                flag=wx.ALIGN_CENTER_VERTICAL)
-        sz.Add(self.bin_text, pos=(0,3), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
-        sz.Add(self.bin, pos=(0,4), span=(1,1), 
+        sz.Add(self.bin_text, pos=(0, 3), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT)
+        sz.Add(self.bin, pos=(0, 4), span=(1, 1),
                flag=wx.ALIGN_CENTER_VERTICAL)
-        sz.Add(self.series_text, pos=(1,0), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
-        sz.Add(self.series, pos=(1,1), span=(1,1), 
+        sz.Add(self.series_text, pos=(1, 0), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT)
+        sz.Add(self.series, pos=(1, 1), span=(1, 1),
                flag=wx.ALIGN_CENTER_VERTICAL)
-        sz.Add(self.scadence_text, pos=(2,0), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
-        sz.Add(self.scadence, pos=(2,1), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL)        
-        sz.Add(self.sname_text, pos=(3,0), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
-        sz.Add(self.sname, pos=(3,1), span=(1,2), 
+        sz.Add(self.scadence_text, pos=(2, 0), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT)
+        sz.Add(self.scadence, pos=(2, 1), span=(1, 1),
                flag=wx.ALIGN_CENTER_VERTICAL)
-        sz.Add(self.autosave_on_text, pos=(3,3), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
-        sz.Add(self.take_series, pos=(1,2), span=(1,1), 
+        sz.Add(self.sname_text, pos=(3, 0), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT)
+        sz.Add(self.sname, pos=(3, 1), span=(1, 2),
                flag=wx.ALIGN_CENTER_VERTICAL)
-        sz.Add(self.autosave_cb, pos=(3,4), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_LEFT)
+        sz.Add(self.autosave_on_text, pos=(3, 3), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT)
+        sz.Add(self.take_series, pos=(1, 2), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL)
+        sz.Add(self.autosave_cb, pos=(3, 4), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_LEFT)
 
-        sz.Add(self.rb_light, pos=(4,1), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
-        sz.Add(self.rb_dark, pos=(4,2), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
-        sz.Add(self.rb_flat, pos=(4,3), span=(1,1), 
-               flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+        sz.Add(self.rb_light, pos=(4, 1), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL)
+        sz.Add(self.rb_dark, pos=(4, 2), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL)
+        sz.Add(self.rb_flat, pos=(4, 3), span=(1, 1),
+               flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL)
 
         self.SetSizerAndFit(sz)
 
@@ -137,63 +142,68 @@ class GuideCamPanel(wx.Panel):
 
     @callafter
     def takePicture(self):
-        #Take Image
+        # Take Image
         i = self.parent.cam.takePictureFITS()
         self.parent.DisplayImage(i)
         self.parent.d.set('zoom to fit')
 
         if self.autosave_cb.GetValue():
-            fdir = datetime.datetime.now().strftime(AUTOSAVE_DIR)
-            fname = datetime.datetime.now().strftime(AUTOSAVE_FNAME)
+            fname = datetime.datetime.now().strftime(
+                self.autosave_directory + AUTOSAVE_FORMAT)
             try:
-                if not os.path.isdir(fdir):
-                    os.makedirs(fdir)
-                    print("Created autosave directory " + fdir)
+                if not os.path.isdir(self.autosave_directory):
+                    os.makedirs(self.autosave_directory)
+                    logging.debug("Created autosave directory "
+                          + self.autosave_directory)
                 with open(fname, 'wb') as image:
                     i.writeto(image)
                     image.close()
-                    print('Image autosaved to ' + fname)
+                    logging.debug('Image autosaved to ' + fname)
             except IOError as e:
-                wx.MessageBox('Warning, image could not be autosaved to %s!' % fname,
-                              'FILE IO ERROR!', wx.OK | wx.ICON_ERROR)
+                wx.MessageBox(
+                    'Warning, image could not be autosaved to %s!' % fname,
+                    'FILE IO ERROR!', wx.OK | wx.ICON_ERROR)
 
     @callafter
     def OnExposeSeries(self, event):
         if self.take_series.GetValue():
-            count   = self.series.GetValue()
+            count = self.series.GetValue()
             cadence = self.scadence.GetValue()
             try:
                 cadence = float(cadence)
             except:
-                wx.CallAfter(wx.MessageBox,'Please select cadence time in seconds!',
+                wx.CallAfter(wx.MessageBox,
+                             'Please select cadence time in seconds!',
                              'INVALID EXPOSURE TIME!', wx.OK | wx.ICON_ERROR)
                 self.endSeries()
                 return
 
             self.take.Enable(False)
             self.take_series.SetLabel('Cancel Series')
-            self.take_series.SetForegroundColour((34,139,34))
+            self.take_series.SetForegroundColour((34, 139, 34))
             self._series_stop = Event()
             self.Series(count, cadence)
         else:
             self._series_stop.set()
 
-            
+
     def OnBin(self, event):
         h = self.bin.GetValue()
-        self.parent.cam.setBinning(h,h)
+        self.parent.cam.setBinning(h, h)
 
 
     def setExposure(self):
-        #Set the exposure time
+        # Set the exposure time
         try:
-            self.parent.cam.setExposure(int(1000*float(self.exposure.GetValue())))
+            self.parent.cam.setExposure(
+                int(1000 * float(self.exposure.GetValue())))
         except:
-            wx.CallAfter(wx.MessageBox,'Please select exposure time in secconds!', 
-                          'INVALID EXPOSURE TIME!', wx.OK | wx.ICON_ERROR)
+            wx.CallAfter(wx.MessageBox,
+                         'Please select exposure time in secconds!',
+                         'INVALID EXPOSURE TIME!', wx.OK | wx.ICON_ERROR)
             raise
 
-    
+
     @run_async(daemon=True)
     def Series(self, count, cadence):
         try:
@@ -209,11 +219,11 @@ class GuideCamPanel(wx.Panel):
             logging.debug("Guide cam series: %i of %i" % (i, count))
             sleep(cadence)
         self.endSeries()
-        
+
 
     @callafter
     def endSeries(self):
         self.take_series.SetLabel("Take Series")
-        self.take_series.SetForegroundColour((0,0,0))
+        self.take_series.SetForegroundColour((0, 0, 0))
         self.take.Enable(True)
         self.take_series.SetValue(False)
